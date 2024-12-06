3ca0d9bc9448c1063a3249869d963cbfd1ddcc33
zee-RGB <zaahir85@gmail.com>
2024-12-07 06:30:23 +1000

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by zee-RGB on 7/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
